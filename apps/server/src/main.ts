abca7d8ca191c9388a81ac35ea69ae4270c99c1f
Anil Kumar <anillakhlan2107@gmail.com>
2025-02-21 21:18:11 +0530

    mongodb connections

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+
+mongoose.connect(`mongodb://127.0.0.1:27017/ScanDine`).then(()=>{
+  console.log("Connected to Mongodb")
+});
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
