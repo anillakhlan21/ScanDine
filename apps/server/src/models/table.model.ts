1f6a1cb521f4fc248ddc1ad6956f7c5c32a6d186
Anil Kumar <anillakhlan2107@gmail.com>
2025-02-22 10:59:12 +0530

    add QR service

---
+++ apps/server/src/models/table.model.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const TableSchema = new mongoose.Schema({
+    tableId: { type: String, required: true, unique: true },
+    qrCodeUrl: { type: String, required: true, unique: true }
+})
+
+export const TableModel = mongoose.model("Table", TableSchema);
---
+++ apps/server/src/services/qr.service.ts
@@ -0,0 +1,16 @@
+import QRCode from 'qrcode';
+import { TableModel } from '../models/table.model';
+
+export class QRService {
+    static async generateQRCode(tableId: string) {
+        try {
+            const orderUrl = `https://abc.com/order?table=${tableId}`
+            const qrCodeUrl = await QRCode.toDataURL(orderUrl);
+            const table = await TableModel.findOneAndUpdate({ tableId }, { qrCodeUrl }, { upsert: true, new: true });
+
+            return qrCodeUrl;
+        } catch (error) {
+            throw new Error("Error generating QR Code")
+        }
+    }
+}
