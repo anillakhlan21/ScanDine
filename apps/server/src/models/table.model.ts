799911976a184d153a4f4ab859eb8fb93fde04a4
Anil Kumar <anillakhlan2107@gmail.com>
2025-02-22 17:28:00 +0530

    add timestamps in table schema

--- apps/server/src/models/table.model.ts
+++ apps/server/src/models/table.model.ts
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const TableSchema = new mongoose.Schema({
   tableId: { type: String, required: true, unique: true },
   qrCodeUrl: { type: String, required: true, unique: true },
-});
+}, { timestamps: true });
 
 export const TableModel = mongoose.model('Table', TableSchema);
