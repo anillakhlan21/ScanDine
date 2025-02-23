71d89ea589ea0cd22878f3621d3096d8ee144e0d
Anil Kumar <anillakhlan2107@gmail.com>
2025-02-23 08:19:16 +0530

    role error handling using middleware

--- apps/server/src/controllers/role.controller.ts
+++ apps/server/src/controllers/role.controller.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { RoleService } from "../services/role.service";
 
 export class RoleController{
-   static async createRole(req: Request, res: Response){
+   static async createRole(req: Request, res: Response, next: NextFunction){
     try {
         const roleData = req.body;
         const newRole = await RoleService.createRole(roleData);
         console.log(newRole);
         return res.status(201).json(newRole);
     } catch (error) {
-        return res.status(500).json({message: 'Not able to create role.'})
+        next(error);
     }
     }
 }   
